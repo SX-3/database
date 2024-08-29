f186780d0d5dc6f086e8155c72219d02cc4789be
SX3 <i4s@ro.ru>
2024-08-29 13:32:22 +0300

    feat: add analitycs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,10 +2,12 @@ import Theme from "vitepress/theme";
 import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
 import "@shikijs/vitepress-twoslash/style.css";
 import type { EnhanceAppContext } from "vitepress";
+import { yandexMetrika } from "@hywax/vitepress-yandex-metrika";
 
 export default {
 	extends: Theme,
-	enhanceApp({ app }: EnhanceAppContext) {
-		app.use(TwoslashFloatingVue);
+	enhanceApp(ctx: EnhanceAppContext) {
+		ctx.app.use(TwoslashFloatingVue);
+		yandexMetrika(ctx, { counter: { id: 98190729 } });
 	},
 };
