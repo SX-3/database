a8acc64aa4d530f84fff89040b80af5ee7683d8c
SX3 <i4s@ro.ru>
2024-08-30 19:34:42 +0300

    fix: favicon

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -7,7 +7,7 @@ const shared = defineConfig({
 	rewrites: {
 		"en/:rest*": ":rest*",
 	},
-	head: [["link", { rel: "icon", href: "/favicon.svg" }]],
+	head: [["link", { rel: "icon", href: "/database/favicon.svg" }]],
 	themeConfig: {
 		socialLinks: [{ icon: "github", link: "https://github.com/SX-3/database" }],
 	},
